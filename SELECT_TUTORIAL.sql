1bcc13cacb5aecf4947ad6e811f54829fb673969
ABDUKHAMID MIRZAAKHMED <133525851+ABDULHAM1D0@users.noreply.github.com>
2026-08-26 21:31:13 +0300

    Add files via upload

---
+++ SELECT_TUTORIAL.sql
@@ -0,0 +1,12 @@
+SELECT * FROM parks_and_recreation.employee_demographics;
+
+
+SELECT first_name, 
+age, 
+age  * 10,
+(age + 10) * 10,
+birth_date 
+FROM parks_and_recreation.employee_demographics;
+
+
+SELECT DISTINCT	first_name, gender FROM parks_and_recreation.employee_demographics;
